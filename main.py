a7ecb63649f3042e9701a7cb86970e69a45c632f
Huy Truong <truongkhaihuy1211@gmail.com>
2023-08-25 13:28:36 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,18 @@
+eat_time = int(input("How many times a week student eat at the student cafeteria?: "))
+price = float(input("The price of a typical student lunch: "))
+grocery_cost = float(input("How much you spend on groceries on a week?: "))
+print("Avarage food expenditure")
+total_weekly = price * eat_time + grocery_cost
+total_daily = total_weekly / 7
+print( f"Total weekly {total_weekly}")
+print( f"Total daily {total_daily}")
+
+
+
+
+
+
+
+
+
+
